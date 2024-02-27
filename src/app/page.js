790974e931e3bf007846b33bad20f3b7252a4ba5
Wijayaac <wijayaac25@gmail.com>
2024-02-28 05:37:24 +0700

    Add : blog posts listing and detail using mdx content

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import BlogSummaryCard from '@/components/BlogSummaryCard';
+import { getBlogPostList } from "@/helpers/file-helpers";
 
-import styles from './homepage.module.css';
+import BlogSummaryCard from "@/components/BlogSummaryCard";
 
-function Home() {
+import styles from "./homepage.module.css";
+import path from "path";
+
+async function Home() {
+  const blogPosts = await getBlogPostList();
   return (
     <div className={styles.wrapper}>
-      <h1 className={styles.mainHeading}>
-        Latest Content:
-      </h1>
-
-      {/* TODO: Iterate over the data read from the file system! */}
-      <BlogSummaryCard
-        slug="example"
-        title="Hello world!"
-        abstract="This is a placeholder, an example which shows how the “BlogSummaryCard” component should be used. You'll want to swap this out based on the data from the various MDX files!"
-        publishedOn={new Date()}
-      />
+      <h1 className={styles.mainHeading}>Latest Content:</h1>
+      {blogPosts.map(({ slug, ...delegated }) => {
+        console.log(delegated);
+        return <BlogSummaryCard key={slug} slug={slug} {...delegated} />;
+      })}
     </div>
   );
 }
