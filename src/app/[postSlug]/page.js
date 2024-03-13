45405f10bd09c7e411ed9942145c05df1d616f59
Wijayaac <wijayaac25@gmail.com>
2024-03-14 06:04:55 +0700

    Add : theme mode toggle functionality

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -3,6 +3,8 @@ import { MDXRemote } from "next-mdx-remote/rsc";
 
 import { loadBlogPost } from "@/helpers/file-helpers";
 
+import { BLOG_TITLE } from "@/constants";
+
 import BlogHero from "@/components/BlogHero";
 import DivisionGroupsDemo from "@/components/DivisionGroupsDemo";
 import CircularColorsDemo from "@/components/CircularColorsDemo";
@@ -10,13 +12,22 @@ import CodeSnippet from "@/components/CodeSnippet";
 
 import styles from "./postSlug.module.css";
 
+export async function generateMetadata({ params }) {
+  const { frontmatter } = await loadBlogPost(params.postSlug);
+
+  return {
+    title: `${frontmatter.title} | ${BLOG_TITLE}`,
+    description: frontmatter.abstract,
+  };
+}
+
 async function BlogPost({ params }) {
   const { frontmatter, content } = await loadBlogPost(params.postSlug);
   return (
     <article className={styles.wrapper}>
       <BlogHero title={frontmatter.title} publishedOn={frontmatter.publishedOn} />
       <div className={styles.page}>
-        <MDXRemote source={content} components={{DivisionGroupsDemo, CircularColorsDemo, pre: CodeSnippet}} />
+        <MDXRemote source={content} components={{ DivisionGroupsDemo, CircularColorsDemo, pre: CodeSnippet }} />
       </div>
     </article>
   );
